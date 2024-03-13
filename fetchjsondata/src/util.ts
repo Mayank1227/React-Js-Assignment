9d6640480f09983357d6825dddcf8a70b90951fe
Mayank1227 <136098348+Mayank1227@users.noreply.github.com>
2024-03-13 11:22:31 +0530

    Delete fetchjsondata directory

--- fetchjsondata/src/util.ts
+++
@@ -1,9 +0,0 @@
-const url = "https://jsonplaceholder.typicode.com/photos";
-const fetchData = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("data fected")
-    return data;
-}
-
-export default fetchData;
